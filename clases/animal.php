192c386b2510d6cfd4c7c2bef3c90d0f3d9d307d
Andres Lopez <lopezpalomo2004@gmail.com>
2026-06-25 17:29:15 -0500

    crear animal

---
+++ clases/animal.php
@@ -0,0 +1,16 @@
+<?php
+
+class perro
+{
+    public function ladrar()
+    {
+        echo "El perro está ladrando. <br>";
+    }
+
+    public function correr()
+    {
+        echo "El perro está corriendo.";
+    }
+}
+
+?>
--- public/index.php
+++ public/index.php
@@ -1,11 +1,19 @@
 <?php
 
 require_once "../clases/persona.php";
+require_once "../clases/animal.php";
 
 $andres = new persona();
-
 $andres->saludar();
 echo "<br>";
 $andres->presentarse();
+
+echo "<br>";
+
+$miPerro = new perro();
+$miPerro->ladrar();
+$miPerro->correr();
+
+
 
 ?>
